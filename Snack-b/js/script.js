a60451e541c01253f1acf05cdd30d8a88a087d5e
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-22 16:52:30 +0100

    fix if user input is over 100 and under 0

--- Snack-b/js/script.js
+++ Snack-b/js/script.js
@@ -13,13 +13,18 @@ createApp({
     },
     methods : {
         generateArrayRandomNumbers() {
-            const item = this.userInput;
-            const apiLink = 'https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=' + item;
-            axios.get(apiLink)
-            .then((result) => {
-                console.log(result.data.response);
-                this.numbersList = result.data.response;
-            })
+            if (this.userInput > 0 && this.userInput <= 100) {
+                const item = this.userInput;
+                const apiLink = 'https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=' + item;
+                axios.get(apiLink)
+                .then((result) => {
+                    console.log(result.data.response);
+                    this.numbersList = result.data.response;
+                })
+            } else {
+                alert('Devi inserire un numero tra 1 e 100!!!');
+            }
+            
         }
     },
 }).mount('#app');
